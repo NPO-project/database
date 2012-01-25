406b0bd664890a32f680229b1f9cecee358c4492
Marlin Cremers <marlinc@mms-projects.net>
2012-01-25 21:09:45 +0100

    Updated the setup script

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mysql -e 'create database npo;'
 echo 'DB_NAME = npo' > config.mk
-echo 'DB_USER = npo' >> config.mk
-echo 'DB_PASS = npo' >> config.mk
+echo 'DB_USER = root' >> config.mk
+echo 'DB_PASS = ' >> config.mk
 echo 'TW_WORLD = NL26' >> config.mk
 echo 'TW_SERVER = NL' >> config.mk
