00b2f76b1a198b7d8fc61cc8318bf735533a3810
Marlin Cremers <marlinc@mms-projects.net>
2012-01-27 20:57:39 +0100

    Fixed the quote problem in the Travis setup file

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mysql --user root -e 'grant usage on *.* to npo@localhost identified by "npo";'
 mysql --user root -e 'grant all privileges on npo.* to npo@localhost;'
-psql -c 'CREATE USER npo WITH PASSWORD npo;' -U postgres
+psql -c "CREATE USER npo WITH PASSWORD 'npo';" -U postgres
 psql -c 'create database npo;' -U postgres
 psql -c 'GRANT ALL PRIVILEGES ON DATABASE npo to npo;' -U postgres
 echo 'DB_NAME = npo' > config.mk
