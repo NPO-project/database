a5cb052763e3ee943e9779afaf55d411cb2ef165
Dennis Degryse <dennisdegryse@gmail.com>
2012-01-28 07:27:12 +0100

    Fixed issue #1: wrong sequence name

--- src/create_main_tables.sql
+++ src/create_main_tables.sql
@@ -10,7 +10,7 @@ CREATE SEQUENCE {PREFIX}members_id_seq;
 
 /* Create table members */
 CREATE TABLE "{PREFIX}members" (
-	"id" integer NOT NULL DEFAULT NEXTVAL('members_id_seq'),
+	"id" integer NOT NULL DEFAULT NEXTVAL('{PREFIX}members_id_seq'),
 	"forum_members_id" integer NULL,
 	"name" varchar(50) NOT NULL,
 	"suspended" boolean NOT NULL DEFAULT FALSE,
